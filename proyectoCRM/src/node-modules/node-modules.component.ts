03e0f394c062d9e055676980a60e4f5a6a5ef95c
AitanaH <aitanur06@gmail.com>
2025-11-22 19:39:35 +0100

    creación carpeta node_modules

---
+++ proyectoCRM/src/node-modules/node-modules.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-node-modules',
+  standalone: true,
+  imports: [],
+  templateUrl: './node-modules.component.html',
+  styleUrl: './node-modules.component.css'
+})
+export class NodeModulesComponent {
+
+}
