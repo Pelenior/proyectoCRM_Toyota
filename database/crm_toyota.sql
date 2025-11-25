1c35da80ef1a82fbfd67611054c211bcea659001
Luna <lunyyuwu@gmail.com>
2025-11-25 19:37:25 +0100

    Database Update

--- database/crm_toyota.sql
+++ database/crm_toyota.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 23, 2025 at 04:06 PM
+-- Generation Time: Nov 25, 2025 at 07:14 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -24,6 +24,50 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `administradores`
+--
+
+DROP TABLE IF EXISTS `administradores`;
+CREATE TABLE `administradores` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(20) NOT NULL DEFAULT '',
+  `email` varchar(40) NOT NULL,
+  `telefono` int(9) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `administradores`
+--
+
+INSERT INTO `administradores` (`id`, `nombre`, `email`, `telefono`) VALUES
+(1, 'Laura Admin', 'ladmin@admin.toyota.com', 896547531),
+(2, 'Eladio Admin', 'eladmin@admin.toyota.com', 159753486);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `choferes`
+--
+
+DROP TABLE IF EXISTS `choferes`;
+CREATE TABLE `choferes` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `nombre` varchar(20) NOT NULL DEFAULT '',
+  `email` varchar(40) NOT NULL,
+  `telefono` int(9) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `choferes`
+--
+
+INSERT INTO `choferes` (`id`, `nombre`, `email`, `telefono`) VALUES
+(1, 'Elcho Ferr', 'elchoferr@toyota.com', 787845457),
+(2, 'Lakon Duc Tora', 'ldtora@toyota.com', 123654789);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `clientes`
 --
 
@@ -32,36 +76,72 @@ CREATE TABLE `clientes` (
   `id` int(10) UNSIGNED NOT NULL,
   `nombre` varchar(20) NOT NULL DEFAULT '',
   `email` varchar(40) NOT NULL,
-  `telefono` int(9) NOT NULL
+  `telefono` int(9) NOT NULL,
+  `id_chofer` int(5) UNSIGNED DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `clientes`
 --
 
-INSERT INTO `clientes` (`id`, `nombre`, `email`, `telefono`) VALUES
-(1, 'Toyota España', 'contacto@toyota.es', 900324578),
-(2, 'Cliente Particular', 'cliente@example.com', 600123456);
+INSERT INTO `clientes` (`id`, `nombre`, `email`, `telefono`, `id_chofer`) VALUES
+(1, 'Toyota España', 'contacto@toyota.es', 900324578, 1),
+(2, 'Cliente Particular', 'cliente@example.com', 600123456, 2);
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `administradores`
+--
+ALTER TABLE `administradores`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `choferes`
+--
+ALTER TABLE `choferes`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `clientes`
 --
 ALTER TABLE `clientes`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_chofer` (`id_chofer`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `administradores`
+--
+ALTER TABLE `administradores`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `choferes`
+--
+ALTER TABLE `choferes`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `clientes`
 --
 ALTER TABLE `clientes`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `clientes`
+--
+ALTER TABLE `clientes`
+  ADD CONSTRAINT `clientes_ibfk_1` FOREIGN KEY (`id_chofer`) REFERENCES `choferes` (`id`) ON DELETE SET NULL;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
