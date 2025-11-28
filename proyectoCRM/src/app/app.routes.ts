9198a6cc9c8b32280c217c2511781bf80d9e45e8
AitanaH <aitanur06@gmail.com>
2025-11-28 10:25:52 +0100

    2 páginas nuevas

--- proyectoCRM/src/app/app.routes.ts
+++ proyectoCRM/src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { FormComponent } from './form/form.component';
 import { HomeComponent } from './home/home.component';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { NosotrosComponent } from './nosotros/nosotros.component'
 
 export const routes: Routes = [
     { path: '', component: HomeComponent},
-    { path: 'form', component: FormComponent }
+    { path: 'form', component: FormComponent },
+    { path: 'servicios', component: ServiciosComponent},
+    { path: 'nosotros', component: NosotrosComponent}
 ];
---
+++ proyectoCRM/src/app/nosotros/nosotros.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './nosotros.component.html',
+  styleUrl: './nosotros.component.css'
+})
+export class NosotrosComponent {
+
+}
+
