ea3774e5037806582dd22e59ea3c4ddb17db2c6f
AitanaH <aitanur06@gmail.com>
2025-11-30 13:33:49 +0100

    solo una página para contratar chófer

--- proyectoCRM/src/app/app.routes.ts
+++ proyectoCRM/src/app/app.routes.ts
@@ -4,7 +4,6 @@ import { ServiciosComponent } from './servicios/servicios.component';
 import { NosotrosComponent } from './nosotros/nosotros.component';
 import { FlotaComponent } from './flota/flota.component';
 import { PreciosComponent } from './precios/precios.component';
-import { ContactoComponent } from './contacto/contacto.component';
 import { AvisoLegalComponent } from './avisoLegal/aviso-legal.component';
 import { PrivacidadComponent } from './privacidad/privacidad.component';
 import { CookiesComponent } from './cookies/cookies.component';
@@ -44,7 +43,6 @@ export const routes: Routes = [
     { path: 'nosotros', component: NosotrosComponent},
     { path: 'flota', component: FlotaComponent},
     { path: 'precios', component: PreciosComponent},
-    { path: 'contacto', component: ContactoComponent},
     { path: 'aviso-legal', component: AvisoLegalComponent},
     { path: 'privacidad', component: PrivacidadComponent},
     { path: 'cookies', component: CookiesComponent},
